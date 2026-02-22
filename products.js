754bbabe225a01ed51635d297ea79ad6d0e5f2d6
Markoha <mr.weflex@gmail.com>
2026-02-22 09:52:48 +0300

    update files

--- products.js
+++ products.js
@@ -3,6 +3,6 @@ const products = [
     { id: 2, name: "Конструктор LEGO", price: 3500, popular: true },
     { id: 3, name: "Радиоуправляемая машинка", price: 2100, popular: false },
     { id: 4, name: "Кукла", price: 1500, popular: true },
-    { id: 5, name: "Настольная игра", price: 900, popular: false }
+    { id: 5, name: "Настольная игра", price: 900, popular: false },
     { id: 6, name: "Лего кружка", price: 1000, popular: false }
 ];
